60bf857c74904847512f525b1531a7e548d34385
Matthieu Deback <matthieu.deback@decathlon.com>
2018-11-20 14:10:38 +0100

    navbar1

--- mattresume/src/modules/navbar/Navbar.js
+++ mattresume/src/modules/navbar/Navbar.js
@@ -5,9 +5,38 @@ import './navbar.css';
 class Navbar extends React.Component {
   render() {
     return (
-      <div className="navbar">
+      <div>
 
-        <h5>test de navbar</h5>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Work experience</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Travels</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Multimedia
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Photos</a>
+          <a class="dropdown-item" href="#">Videos</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Concert</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 
       </div>
     );
