2ccb2412afb31f7a893b9274cc3dfb24226b45b7
Christophe Pesin <christophe.pesin@gmail.com>
2018-12-15 13:54:51 +0100

    Fix background

--- src/modules/firstpage/Firstpage.js
+++ src/modules/firstpage/Firstpage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './firstpage.css';
 
-
-//modules
 import Navbar from "../navbar/Navbar";
 
 let curly = require ("./images/curly.jpg")
@@ -10,36 +8,28 @@ let curly = require ("./images/curly.jpg")
 class Firstpage extends React.Component {
   render() {
     return (
+      
+      <div>
+        <div class="snow">&nbsp;</div>
+        <div class="snow2">&nbsp;</div>
+        <div class="fill">
+          <Navbar />
 
-      <div class="snow2 fill">
-      <Navbar />
+          <div class="page">
+            <div class="element">
+              <p class="text">
+                Hi! Welcome to my online resume!<br/>
+                My name is Matt! <br/>
+                Enjoy your visit on this website!
+              </p>
 
-      <div class="page">
-      <div class="element">
-      <p class="text">
-      TEST Hi! Welcome to my online resume!<br/>
-      My name is Matt! <br/>
-      Enjoy your visit on this website!
-      </p>
+              <img src={curly} className="profileimg" alt=""/>
 
-      <img src={curly} className="profileimg" alt=""/>
-
-
-
-
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
 
-
-
-
-
-
-
-
-
-
-      </div>
     );
   }
 }
--- src/modules/navbar/Navbar.js
+++ src/modules/navbar/Navbar.js
@@ -6,32 +6,29 @@ class Navbar extends React.Component {
   render() {
     return (
       <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Matthieu Deback</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Matthieu Deback</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://www.linkedin.com/in/matthieudeback/">LinkedIn</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/pic">Pictures</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Music</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" target="_blank" href="https://www.linkedin.com/in/matthieudeback/">LinkedIn</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pictures</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Music</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
